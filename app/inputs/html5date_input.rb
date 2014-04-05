f0c0ec7818dae8b752a6d3610bcfecd38c9a9ab4
ashrestha <ashrestha@uchicago.edu>
2014-04-04 21:08:41 -0400

    Fix time and date fields

--- app/inputs/html5date_input.rb
+++ app/inputs/html5date_input.rb
@@ -1,6 +1,5 @@
 class Html5dateInput < SimpleForm::Inputs::Base
   def input
-    @builder.date_field(attribute_name, input_html_options) + \
-    @builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"}) 
+    @builder.date_field(attribute_name, input_html_options)
   end
 end
--- app/inputs/html5time_input.rb
+++ app/inputs/html5time_input.rb
@@ -1,6 +1,5 @@
 class Html5timeInput < SimpleForm::Inputs::Base
   def input
-    @builder.time_field(attribute_name, input_html_options) + \
-    @builder.hidden_field(attribute_name, { :class => attribute_name.to_s + "-alt"}) 
+    @builder.time_field(attribute_name, input_html_options)
   end
 end
